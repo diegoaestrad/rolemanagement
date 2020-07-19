96f275f56991000eddb1b7ff49b66eda3a3f9272
Diego Estrada Beltran <200427046@student.georgianc.on.ca>
2020-07-19 19:20:43 -0400

    https://github.com/diegoaestrad/rolemanagement rolemanagement part1

---
+++ rolemanagement/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using rolemanagement.Data;
+
+[assembly: HostingStartup(typeof(rolemanagement.Areas.Identity.IdentityHostingStartup))]
+namespace rolemanagement.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
